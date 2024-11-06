3378a7c3fd02bec4f04e9081e239c966120b06d9
celiamartinelli <celia.martinelli@oclock.school>
2024-11-06 18:25:04 +0100

    fix-bug-homaPage

--- app/page.tsx
+++ app/page.tsx
@@ -5,39 +5,42 @@ export default async function Index() {
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
         <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-          <Link
-            href="https://www.facebook.com/profile.php?id=61561436403399&locale=fr_FR"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className="mb-3 text-2xl font-semibold">
-              Facebook{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className="m-0 max-w-[30ch] text-sm opacity-50">
-              Découvrer notre page facebook
-            </p>
-          </Link>
-
-          <Link
-            href="https://www.helloasso.com/associations/ecole-des-chats-du-pays-houdanais/formulaires/1"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className="mb-3 text-2xl font-semibold">
-              Hello Asso{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className="m-0 max-w-[30ch] text-sm opacity-50">
-              Merci pour eux!
-            </p>
-          </Link>
+          <div>
+            <Link
+              href="https://www.facebook.com/profile.php?id=61561436403399&locale=fr_FR"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className="mb-3 text-2xl font-semibold">
+                Facebook{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                Découvrer notre page facebook
+              </p>
+            </Link>
+          </div>
+          <div>
+            <Link
+              href="https://www.helloasso.com/associations/ecole-des-chats-du-pays-houdanais/formulaires/1"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className="mb-3 text-2xl font-semibold">
+                Hello Asso{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                Merci pour eux!
+              </p>
+            </Link>
+          </div>
         </div>
       </main>
     </>

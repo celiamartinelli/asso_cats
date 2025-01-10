7569f6b1c98362f388486912e344bac1ede389e9
celiamartinelli <celia.martinelli@oclock.school>
2025-01-10 18:56:36 +0100

    add-section-in-homePage

---
+++ app/(public)/calendar/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Calendar() {
+  return (
+    <div className="p-6 bg-gray-100">
+      <h1 className="text-3xl font-bold mb-4">Calendrier</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { getImportantDates } from "@/utils/actions";
+import { Button } from "@/components/ui/button";
 
 interface ImportantDate {
   calendar_id: string;
@@ -37,6 +38,23 @@ export default function Index() {
                 <p className="opacity-50">Collecte</p>
               </li>
             </ul>
+            <Button type="button" className="mt-8">
+              <Link href="/calendar">Voir toutes les dates</Link>
+            </Button>
+          </div>
+          <div className="border-black border-y-2 py-14 p-4 w-full lg:col-span-2 mb-4 dark:border-white">
+            <h3>Sur Quel secteur on agit? et quel commune</h3>
+
+            <Button type="button" className="mt-8">
+              <Link href="/municipality">Voir la liste complète</Link>
+            </Button>
+          </div>
+          <div className="border-black border-y-2 py-14 p-4 w-full lg:col-span-2 mb-4 dark:border-white">
+            <h3>Comment se déroule une adoption</h3>
+            <p>fresque etape adoption</p>
+            <Button type="button" className="mt-8">
+              <Link href="/adoption">Voir les Amours</Link>
+            </Button>
           </div>
           <div className="flex border-black border-y-2 py-14 p-4 w-full lg:col-span-2 mb-4 dark:border-white">
             <Link
@@ -70,6 +88,38 @@ export default function Index() {
               </h2>
               <p className="m-0 max-w-[30ch] text-sm opacity-50">
                 Merci pour eux!
+              </p>
+            </Link>
+            <Link
+              href="https://www.instagram.com/ecoledeschats/"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className="mb-3 text-2xl font-semibold">
+                Instagram{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                Découvrer notre Instagram
+              </p>
+            </Link>
+            <Link
+              href="https://www.linkedin.com/groups/13126906/"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className="mb-3 text-2xl font-semibold">
+                Linkedin{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                Découvrer notre page Linkedin
               </p>
             </Link>
           </div>

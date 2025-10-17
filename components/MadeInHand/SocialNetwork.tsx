28f1bb9a54b2766756ac941e8b26563ece4cb0af
celiamartinelli <celia.martinelli@oclock.school>
2025-10-17 20:18:31 +0200

    fix: amélioration de la page adoption

--- components/MadeInHand/SocialNetwork.tsx
+++ components/MadeInHand/SocialNetwork.tsx
@@ -20,7 +20,7 @@ export default function SocialNetwork() {
           <Instagram size={28} />
         </Link>
         <Link
-          href="https://www.linkedin.com/company/association-l-%C3%A9cole-des-chats-du-pays-houdanais/posts/?feedView=allhttps://www.linkedin.com/company/association-l-%C3%A9cole-des-chats-du-pays-houdanais"
+          href="https://www.instagram.com/ecoledeschatsdupayshoudanais/"
           className="hover:bg-accent hover:rounded-lg p-2 hover:text-black"
           target="_blank"
         >

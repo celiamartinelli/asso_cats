bcf0552655e99e7a472318e4509326ec25d74912
celiamartinelli <celia.martinelli@oclock.school>
2025-10-19 17:34:35 +0200

    fix bug link externe

--- components/MadeInHand/SocialNetwork.tsx
+++ components/MadeInHand/SocialNetwork.tsx
@@ -20,7 +20,7 @@ export default function SocialNetwork() {
           <Instagram size={28} />
         </Link>
         <Link
-          href="https://www.instagram.com/ecoledeschatsdupayshoudanais/"
+          href="https://www.linkedin.com/company/association-l-%C3%A9cole-des-chats-du-pays-houdanais/?viewAsMember=true"
           className="hover:bg-accent hover:rounded-lg p-2 hover:text-black"
           target="_blank"
         >

5bd3a0e542642f8e5b67fe39208508e12c9db440
celiamartinelli <celia.martinelli@oclock.school>
2025-10-21 21:10:24 +0200

    add

--- components/MadeInHand/Header.tsx
+++ components/MadeInHand/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import NavBar from "./Client/NavBar";
+
 import { Button } from "@/components/ui/button";
 import { buttonVariants } from "@/components/ui/button";
 

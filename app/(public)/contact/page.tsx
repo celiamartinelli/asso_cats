7d4bbccefd66465c446fc652ecec2be6acd62f5f
celiamartinelli <celia.martinelli@oclock.school>
2025-10-21 20:06:23 +0200

    supprimer lien

--- app/(public)/contact/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Contact() {
-  return (
-    <div className="p-6 bg-gray-100">
-      <h1 className="text-3xl font-bold mb-4">Contact</h1>
-    </div>
-  );
-}
--- app/(public)/helpthem/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function HelpThem() {
-  return (
-    <div className="p-6 bg-gray-100">
-      <h1 className="text-3xl font-bold mb-4">help them</h1>
-    </div>
-  );
-}
